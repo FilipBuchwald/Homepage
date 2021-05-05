f37c83097c3fd15cbe35be3b978a4a4d4b34b07f
Filip Buchwald <fixu1998@gmail.com>
2021-05-06 00:15:10 +0200

    refactor js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
-console.log("Witaj w konsoli");
-let image = document.querySelector(".image");
-
-let button = document.querySelector(".js-image__button");
-
-button.addEventListener("click", () => {
-    if (button.innerText === "Następne zdjęcie") {
-        button.innerText = "Poprzednie zdjęcie";
-        image.src = "https://i.postimg.cc/FzPnyTH5/sunset.jpg";
-        image.alt = "Mazury";
+{
+    const welcome= () => {
+        console.log("Witaj w konsoli");
     }
-    else {
-        button.innerText = "Następne zdjęcie";
-        image.src = "https://i.postimg.cc/sxJTVwhG/Szko-a-Jazdy-Subaru.jpg";
-        image.alt = "Subaru";
+    
+    const changeImageButton = () => {      
+        
+        if (button.innerText === "Następne zdjęcie") {
+            button.innerText = "Poprzednie zdjęcie";
+            image.src = "https://i.postimg.cc/FzPnyTH5/sunset.jpg";
+            image.alt = "Mazury";
+        }
+        else {
+            button.innerText = "Następne zdjęcie";
+            image.src = "https://i.postimg.cc/sxJTVwhG/Szko-a-Jazdy-Subaru.jpg";
+            image.alt = "Subaru";
+        }
+    
+    }
+    
+    const  init = () => {  
+    button.addEventListener("click", changeImageButton);
+    welcome();
 
     }
-
-});
+    
+    const button = document.querySelector(".js-image__button");
+    const image = document.querySelector(".image"); 
+    
+    init();
+}
